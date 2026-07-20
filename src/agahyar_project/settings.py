5a46603e7de70bd8181aa812f55cca802d6f9479
CodeWriter21 <CodeWriter21@gmail.com>
2026-07-20 09:21:01 +0330

    chore: bump version to 1.3.0

--- src/agahyar_project/settings.py
+++ src/agahyar_project/settings.py
@@ -358,6 +358,6 @@ REST_FRAMEWORK = {
 SPECTACULAR_SETTINGS = {
     "TITLE": "Agahyar API",
     "DESCRIPTION": "REST API for the Agahyar smart citizen information system.",
-    "VERSION": "1.2.0",
+    "VERSION": "1.3.0",
     "SERVE_INCLUDE_SCHEMA": False,
 }
